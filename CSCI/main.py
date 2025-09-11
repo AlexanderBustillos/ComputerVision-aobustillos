137380b4835bf4a6e405f91bf54ecda2b8561bef
Alex <alexanderbustillos1@gmail.com>
2025-09-10 22:49:37 -0600

    Pushing code maybe?

---
+++ CSCI/main.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+
+def main() -> None:
+    img = cv2.imread("images\OpenCV_Logo.jpg", 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+   main()
