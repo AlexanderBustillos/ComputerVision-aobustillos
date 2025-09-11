bee8ca96d6d7e9dbcc106b799e24c7feaf81121e
Alex <alexanderbustillos1@gmail.com>
2025-09-10 22:59:45 -0600

    I may be learning

--- CSCI/main.py
+++ CSCI/main.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def main() -> None:
+    #Opening the image with imread
     img = cv2.imread("images\OpenCV_Logo.jpg", 1)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imshow("image", img)
